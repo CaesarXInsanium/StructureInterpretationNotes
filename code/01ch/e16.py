9aae19d977e004f53f83d029b278f9da189b7862
Caesar X Insanium <caesarxinsanium.dev@gmail.com>
2022-11-05 20:29:30 -0500

    stole some code

--- code/01ch/e16.py
+++ code/01ch/e16.py
@@ -19,15 +19,50 @@ def f(b, n):
         return b * f(b, n-1)
 
 
-def test(b, n):
-    correct = pow(b, n)
-    recursive = f(b, n)
-    print(f"Corect: {correct}\tRecursive: {recursive}")
+def g(b, n):
+    m = n
+    result = b
+    if n == 0:
+        return 1
+    elif b == 0:
+        return 0
+    elif n == 1:
+        return b
+    elif n == 2:
+        return square(b)
+    else:
+        while m != 1:
+            print(f"m = {m}, result = {result}", end="\t")
+            if even(m):
+                result = square(b) * result
+                m = int(m / 2)
+            else:
+                result = b * result
+                m = m - 1
+            print(f"m = {m}, result = {result}")
+    return result
 
+
+
+def iter_fast_expt( b ,n):
+    def iter(N, B , A):
+        if n == 0:
+            return A
+        elif even(n):
+            return iter(n / 2, square(B), A)
+        else:
+            iter(n -1, B, B * A)
+    return iter(n,b,1)
+
+def big_test(r):
+    for b in range(r):
+        for n in range(r):
+            result = iter_fast_expt(b, n)
+            correct = pow(b, n)
+            print(f"b = {b}, n = {n}\tresult = {result}\tcorect = {correct}")
+            assert(correct == result)
+            
 
 if __name__ == "__main__":
-    test(2, 2)
-    test(2, 0)
-    test(4, 5)
-    test(4, 3)
-    test(1, 9)
+        
+    big_test(10)
