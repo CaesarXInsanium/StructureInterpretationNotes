705c36628dd3ba6a9683b935e6c9c6ab1ffc4054
Caesar X Insanium <jesusramos.official@gmail.com>
2023-02-18 20:55:20 -0600

    ransadas

--- code/02ch/trees.scm
+++ code/02ch/trees.scm
@@ -1,5 +1,5 @@
 (define (count-leaves x)
-  (cond ((null? x) o)
+  (cond ((null? x) 0)
         ((not (pair? x)) 1)
         (else (+ (count-leaves (car x))
                  (count-leaves (cdr x))))))
