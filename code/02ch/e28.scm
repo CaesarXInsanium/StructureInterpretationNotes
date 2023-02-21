60aa1394ea43ee98f830b1d968d7cf8839fbacdd
Ceasar X Insanium <jesramos.official@gmail.com>
2023-02-20 20:54:33 -0600

    finished exe2.28

--- code/02ch/e28.scm
+++ code/02ch/e28.scm
@@ -29,3 +29,30 @@
 (define x (list (list 1 2) (list 3 4)))
 (define y (list 1))
 (define z (list 3 (list 4 5)))
+
+(define (fringe items)
+  (if (null? items)
+      nil
+      (cons (if (list? (car items))
+                (fringe (car items))
+                (car items))
+            (fringe (cdr items)))))
+
+(define (fringe items)
+  (if (and (not (null? items)) list? items)
+      (let ((head (car items)))
+        (cond ((list? head) (append (fringe head) 
+                                    (fringe (cdr items))))
+              ((number? head) (append (list head) 
+                                      (if (null? (cdr items)) 
+                                          nil 
+                                          (fringe (cdr items)))))
+              (else nil)))
+      nil))
+
+(fringe x)
+(fringe y)
+(fringe z)
+(fringe (list 1 (list 2)))
+(define my-tree (list 1 (list 2 (list 3 4) (list 5 6)) (list 7 (list 8)))) 
+(fringe my-tree)
