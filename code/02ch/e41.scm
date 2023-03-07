8e5be9bbd5cb53c63f813e28c1f9ea0db2dddfe1
Ceasar X Insanium <jesramos.official@gmail.com>
2023-03-06 19:34:35 -0600

    managed to solve this silly exercise 2.41

--- code/02ch/e41.scm
+++ code/02ch/e41.scm
@@ -4,14 +4,21 @@
 
 (define (accumulate op initial sequence)
   (if (null? sequence)
-      initial
-      (op (car sequence)
-          (accumulate op initial (cdr sequence)))))
+      initial (op (car sequence)
+                  (accumulate op initial (cdr sequence)))))
 
 (define (enumurate-interval low high)
   (if (> low high)
       nil
       (cons low (enumurate-interval (+ low 1) high))))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
 
 ;; Begin
 
@@ -25,11 +32,22 @@
                    (enumurate-interval 1 n))))
 
 (define (gen n)
-  (accumulate cons nil (map (lambda (pair x)
-                              (append pair x))
-                            (gen-pairs n)
-                            (enumurate-interval 1 n))))
+  (let ((pairs (gen-pairs n))
+        (interval (enumurate-interval 1 n)))
+    (accumulate append
+                nil
+                (map (lambda (pair)
+                       (map (lambda (int)
+                              (list (list-ref pair 0) (list-ref pair 1) int))
+                            interval))
+                     pairs))))
 
-(list? (gen-pairs 3))
-(display (gen 3))
+(define (triples n s)
+  (filter (lambda (triple)
+            (= s (accumulate + 0 triple)))
+          (gen n)))
+
+(triples 10 5)
+(triples 13 7)
+(display result)
 (newline)
