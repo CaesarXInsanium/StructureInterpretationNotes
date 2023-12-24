bb42dff22113490d692406fc1f40f96cbce095c8
Caesar X Insanium <caesarxinsanium.dev.@gmail.com>
2023-12-24 10:52:43 -0600

    2.61 was relatively easy

---
+++ code/02ch/e61.scm
@@ -0,0 +1,31 @@
+(define false #f)
+(define true #t)
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1))
+            (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cond ((= x1 x2)
+                      (cons x1
+                            (intersection-set (cdr set1)
+                                              (cdr set2))))
+                     ((< x1 x2)
+                      (intersection-set (cdr set1) set2))
+                     ((< x2 x1)
+                      (intersection-set set1 (cdr set2)))))))))
+;; start exercise
+
+(define (adjoin-set x set)
+  (cond ((null? set) (cons x '()))
+        ((= (car set) x) set)
+        ((< x (car set)) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
--- org/code/sets.scm
+++ org/code/sets.scm
@@ -12,9 +12,18 @@
       set
       (cons x set)))
 
+;; elements that exist in both sets
 (define (intersection-set set1 set2)
   (cond ((or (null? set1) (null? set2)) '())
         ((element-of-set? (car set1) set2)
          (cons (car set1)
                (intersection-set (cdr set1) set2)))
         (else (intersection-set (cdr set1) set2))))
+
+;; Answer to 2.59, add elements in both sets into one
+(define (union-set a b)
+  (cond ((or (null? a) (null? b)) '())
+        ((element-of-set? (car a) b)
+         (union-set (cdr a) b))
+        (else (cons (car a)
+                    (union-set (cdr a) b)))))
